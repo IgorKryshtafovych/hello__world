281ec66d2293d8d29791d4b2c0e8bc61e52169b6
IgorKryshtafovych <i.krishtafovich@gmail.com>
2022-07-19 22:33:09 +0300

    Added additional word to console ptint

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,8 @@ package main.java;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello GoIT World");
+        System.out.print("Hello GoIT World");
+        System.out.print(" Hello");
+        System.out.print(" Hello");
     }
 }
