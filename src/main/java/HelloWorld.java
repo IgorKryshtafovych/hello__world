60dac00c23a2d87411a39389965682f17a28f3d7
IgorKryshtafovych <i.krishtafovich@gmail.com>
2022-07-20 17:57:00 +0300

    Added equals method for strings

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,5 +5,10 @@ public class HelloWorld {
         System.out.print("Hello GoIT World");
         System.out.print(" Hello");
         System.out.print(" Hello");
-    }
+
+
+    String name1 = "Igor";
+    String name2 = "Igor";
+    System.out.println(name1.equals(name2));
+}
 }
